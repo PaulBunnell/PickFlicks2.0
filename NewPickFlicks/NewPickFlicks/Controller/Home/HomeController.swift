24c03791dced86dacb30e6ef893487c52c7ca6f6
Paul Bunnell <bunnellrpaul@gmail.com>
2021-03-25 08:34:13 -0600

    Adding API to card view
    Implemented API into card view swipping.

--- NewPickFlicks/NewPickFlicks/Controller/Home/HomeController.swift
+++ NewPickFlicks/NewPickFlicks/Controller/Home/HomeController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class HomeController: UIViewController {
     
@@ -34,14 +35,15 @@ class HomeController: UIViewController {
         return view
     }()
     
+    let movieController = MovieController()
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureUI()
         configureCards()
-        
+        configureUI()
         view.addSubview(visualEffectView)
         visualEffectView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
 
@@ -55,35 +57,20 @@ class HomeController: UIViewController {
     
     func configureCards() {
         
-//        cardView.delegate = self
-                
-        let movie1 = Movies(name: "King Kong", rating: "PG-13", category: "Adventure/Fantasy", images: [#imageLiteral(resourceName: "King-Kong-2005-movie-poster-709x1024"), #imageLiteral(resourceName: "4562400e6b92705125b2b8ef458b6603")])
-        let movie2 = Movies(name: "The Dark Night", rating: "PG-13", category: "Action/Adventure", images: [#imageLiteral(resourceName: "dark_knight_2008_graffiti_teaser_original_film_art_db2955b9-07ea-47b9-94fa-abf3e9e594a2_5000x"), #imageLiteral(resourceName: "MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_UY1200_CR90,0,630,1200_AL_"), #imageLiteral(resourceName: "c6636d059525d610bbca10afa36a125d"), #imageLiteral(resourceName: "976d4a125f485f771d70567c2c90ec70")])
-        let movie3 = Movies(name: "Back to the Future", rating: "PG", category: "Sci-fi/Comedy", images: [#imageLiteral(resourceName: "39b39a4615f98ed1174f61a0f910e00b")])
-        let movie4 = Movies(name: "The little mermaid", rating: "G", category: "Family/Musical", images: [#imageLiteral(resourceName: "MV5BN2JlZTBhYTEtZDE3OC00NTA3LTk5NTQtNjg5M2RjODllM2M0XkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_")])
-        let movie5 = Movies(name: "Braveheart", rating: "R", category: "War/Drama", images: [#imageLiteral(resourceName: "51PvUkCVqOL._AC_"), #imageLiteral(resourceName: "e0c580fdd7a6191f7873b6071095117d_a8de0a69ff17e925e8233498ae3bc2d1"), #imageLiteral(resourceName: "282339__65385.1342528156.380.500")])
-        let movie6 = Movies(name: "Forrest Gump", rating: "PG-13", category: "Romance/Drama", images: [#imageLiteral(resourceName: "s-l640")])
+        movieController.fetchItems { (movies) in
         
-        let cardView1 = CardView(viewModel: CardViewModel(movies: movie1))
-        let cardView2 = CardView(viewModel: CardViewModel(movies: movie2))
-        let cardView3 = CardView(viewModel: CardViewModel(movies: movie3))
-        let cardView4 = CardView(viewModel: CardViewModel(movies: movie4))
-        let cardView5 = CardView(viewModel: CardViewModel(movies: movie5))
-        let cardView6 = CardView(viewModel: CardViewModel(movies: movie6))
+            DispatchQueue.main.async {
+                for movie in movies {
+                        
+                    let newCardView = CardView(viewModel: CardViewModel(movie: movie))
+                        
+                    self.deckView.addSubview(newCardView)
+                        
+                    newCardView.fillSuperview()
+                }
+            }
+        }
         
-        deckView.addSubview(cardView1)
-        deckView.addSubview(cardView2)
-        deckView.addSubview(cardView3)
-        deckView.addSubview(cardView4)
-        deckView.addSubview(cardView5)
-        deckView.addSubview(cardView6)
-        
-        cardView1.fillSuperview()
-        cardView2.fillSuperview()
-        cardView3.fillSuperview()
-        cardView4.fillSuperview()
-        cardView5.fillSuperview()
-        cardView6.fillSuperview()
     }
     
     func configureUI() {
@@ -124,7 +111,8 @@ extension HomeController: HomeNavigationStackViewDelegate {
             }))
 
             alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: { (_) in
-                print("User click Dismiss button")
+                print("User click Dismiss button".uppercased())
+                
             }))
 
             self.present(alert, animated: true, completion: {
@@ -137,16 +125,13 @@ extension HomeController: HomeNavigationStackViewDelegate {
     }
 }
 
-
 //MARK: - cardViewDelegate
 
 extension HomeController: cardViewDelegate {
-    func cardView(_ view: CardView, wantToShowProfileFor movies: Movies) {
-        let controller = MovieDetailController(movies: movies)
+    func cardView(_ view: CardView, wantToShowProfileFor movie: Movie) {
+        let controller = UIHostingController(rootView: MovieDetailView())
         controller.modalPresentationStyle = .fullScreen
         present(controller, animated: true, completion: nil)
-        
-        print("DEBUG: Show details")
         
     }
 }
@@ -164,7 +149,7 @@ extension HomeController: BottomControlStackViewDelegate {
     
     func handleStartSession() {
         print("DEBUG: Handlo startSession here...")
-        
+                
         view.addSubview(popUpWindow)
         popUpWindow.fillSuperview()
     }
